1ef8dca6601941d04d09d345d62fee549fc2dabc
ritvika1605 <ritvika16@outlook.com>
2021-08-14 20:16:22 +0530

    Updates

--- precache-manifest.e7e649b076c6d4a3c7e70981e1609bcd.js
+++ precache-manifest.e7e649b076c6d4a3c7e70981e1609bcd.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "d23b602daf741e7239fe",
-    "url": "/me/static/css/main.a99bdc09.chunk.css"
+    "revision": "053d365e13f74c1ba81c",
+    "url": "/me/static/css/main.ccb428a3.chunk.css"
   },
   {
-    "revision": "d23b602daf741e7239fe",
-    "url": "/me/static/js/main.d23b602d.chunk.js"
+    "revision": "053d365e13f74c1ba81c",
+    "url": "/me/static/js/main.053d365e.chunk.js"
   },
   {
     "revision": "1caf0789ee8fdb1f0b3c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/me/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "3d6a2bd3d31ce0bb8704ca1f6db34182",
+    "revision": "33a5b0ae552f939b0b057ae1f3f59f1a",
     "url": "/me/index.html"
   }
 ];
